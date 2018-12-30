0d52ae6b5a44b91b529dd153109c67c58964a290
kcluniesa <kcluniesa@yahoo.com>
2018-12-30 16:46:42 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,29 +9,29 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   binding.pry
 
-end
+#end
 
-create_project_hash
+#create_project_hash
   
- # projects = {}
+  projects = {}
  
-  # Iterate through the projects
+   #Iterate through the projects
 
-# kickstarter.css("li.project.grid_4").each do |project|
+ kickstarter.css("li.project.grid_4").each do |project|
  
- #   title = project.css("h2.bbcard_name strong a").text
-  #  projects[title.to_sym] = {
-  #    :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
-   #   :description => project.css("p.bbcard_blurb").text,
-    #  :location => project.css("ul.project-meta span.location-name").text,
-    #  :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-  #  }
-#  end
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
+  end
 
    #return the projects hash
-#  projects
+  projects
   
-#end
+end
 
 #binding.pry
 #create_project_hash
